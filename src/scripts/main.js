90774e549a1555e048642f11bb4119583b0f39a6
Zarva <zarva6596@gmail.com>
2020-09-28 09:47:25 +0300

    added slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,25 @@
+/* eslint-disable no-undef */
 'use strict';
+
+$(function() {
+  $('.partners').slick({
+    arrows: false,
+    slidesToShow: 3,
+    autoplay: true,
+    // variableWidth: true,
+    // centerMode: true,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: { slidesToShow: 2 },
+      },
+    ],
+  });
+
+  $('.partners').css('border', '1px solid red');
+
+  $('.home-support__slider').slick({
+    fade: true,
+    speed: 2000,
+  });
+});
